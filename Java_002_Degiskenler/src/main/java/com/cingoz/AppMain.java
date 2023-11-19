cef9030443d7e305c895bbba9365e593ec696990
oguzhan-cingoz <76851381+oguzhan-cingoz@users.noreply.github.com>
2023-11-19 20:09:41 +0300

    delete project

--- Java_002_Degiskenler/src/main/java/com/cingoz/AppMain.java
+++
@@ -1,9 +0,0 @@
-package com.cingoz;
-
-public class AppMain {
-
-    public static void main(String[] args) {
-        System.out.println("Selam");
-    }
-
-}
--- Java_005_KararKontrolMekanizmalari/src/main/java/com/cingoz/AppMain.java
+++
@@ -1,7 +0,0 @@
-package com.cingoz;
-
-public class AppMain {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
